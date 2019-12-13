6ab00b2a828452ee8d597643650ad095296120a6
Thorben Groos <thorben.groos@student.uni-siegen.de>
2019-12-13 15:40:29 +0100

    Added vaiable f and prepared Constraint 2 to 9 for fuel stations.

--- src/logic/model.java
+++ src/logic/model.java
@@ -7,12 +7,17 @@ import ilog.concert.*;
 public class model {
 
 	/**
-	 * Here you can change the number of users.
+	 * Number of Pick up nodes
 	 */
 	private static int n;
+	
+	/**
+	 * Number of AFSs
+	 */
+	private static int f;
 	private static IloCplex cplex;
 	private static IloNumVar[][][] x;
-	private static Node[] N;
+	private static Node[] V;
 	private static Truck[] K;
 
 	private static IloNumVar[][] B;
@@ -39,18 +44,18 @@ public class model {
 															// infeasibility column 'Q(i1;k2)'.
 
 		// c enthält die Distanz zwischen allen Knoten
-		double[][] c = new double[N.length][N.length];
+		double[][] c = new double[V.length][V.length];
 		// t enthält die Fahrzeit zwischen allen Knoten.
-		double[][] t = new double[N.length][N.length];
+		double[][] t = new double[V.length][V.length];
 
 		double xDistance;
 		double yDistance;
 
-		for (int i = 0; i < N.length; i++) {
-			for (int j = 0; j < N.length; j++) {
+		for (int i = 0; i < V.length; i++) {
+			for (int j = 0; j < V.length; j++) {
 				if (i != j) {
-					xDistance = Math.pow(N[i].getxPosition() - N[j].getxPosition(), 2);
-					yDistance = Math.pow(N[i].getyPosition() - N[j].getyPosition(), 2);
+					xDistance = Math.pow(V[i].getxPosition() - V[j].getxPosition(), 2);
+					yDistance = Math.pow(V[i].getyPosition() - V[j].getyPosition(), 2);
 					c[i][j] = Math.sqrt(xDistance + yDistance);
 
 					// Die Fahrzeit zwischen i und j ist die Entfernung zwischen den Knoten * 60.
@@ -63,9 +68,9 @@ public class model {
 			cplex = new IloCplex();
 
 			// Binary decision variable.
-			x = new IloNumVar[N.length][N.length][K.length];
-			for (int i = 0; i < N.length; i++) {
-				for (int j = 0; j < N.length; j++) {
+			x = new IloNumVar[V.length][V.length][K.length];
+			for (int i = 0; i < V.length; i++) {
+				for (int j = 0; j < V.length; j++) {
 					if (i != j) {
 						for (int k = 0; k < K.length; k++) {
 							x[i][j][k] = cplex.boolVar("x(" + "i" + i + ";j" + j + ";k" + k + ")");
@@ -76,8 +81,8 @@ public class model {
 
 			// Zielfunktion
 			IloLinearNumExpr obj = cplex.linearNumExpr();
-			for (int i = 0; i < N.length; i++) {
-				for (int j = 0; j < N.length; j++) {
+			for (int i = 0; i < V.length; i++) {
+				for (int j = 0; j < V.length; j++) {
 					if (i != j) {
 						for (int k = 0; k < K.length; k++) {
 							obj.addTerm(c[i][j], x[i][j][k]);
@@ -95,10 +100,10 @@ public class model {
 			// Cordeau geht von 1..n, Pesch von 1..2n
 			// Von 1..n ist schneller aus von 1..2n.
 			// Beides funktioniert.
-			for (int i = 1; i <= n; i++) {
+			for (int i = 1; i <= 2*n; i++) {
 				IloLinearNumExpr expr = cplex.linearNumExpr();
 				for (int k = 0; k < K.length; k++) {
-					for (int j = 0; j < N.length; j++) {
+					for (int j = 0; j < V.length; j++) {
 						if (i != j) {
 							expr.addTerm(1.0, x[i][j][k]);
 						}
@@ -106,73 +111,95 @@ public class model {
 				}
 				cplex.addEq(expr, 1.0, "Constraint2");
 			}
-
-			// Constraint 3: visit pickup and dropdown depot by the same vehicle.
-			for (int i = 1; i <= n; i++) {
+			
+			// Constraint 3 Pesch: Es darf nur eine Kante in einen pick up oder dropdown Node führen.
+			for (int j = 1; j <= 2*n; j++) {
+				IloLinearNumExpr expr = cplex.linearNumExpr();
 				for (int k = 0; k < K.length; k++) {
-					IloLinearNumExpr expr = cplex.linearNumExpr();
-					for (int j = 0; j < N.length; j++) {
+					for (int i = 0; i < V.length; i++) {
 						if (i != j) {
 							expr.addTerm(1.0, x[i][j][k]);
 						}
 					}
-					for (int j = 0; j < N.length; j++) {
+				}
+				cplex.addEq(1.0, expr, "Constraint3");
+			}
+
+			// Constraint 4 Pesch: visit pickup and dropdown depot by the same vehicle.
+			// Constraint 3 Cordeau
+			for (int k = 0; k < K.length; k++) {
+				for (int i = 1; i <= n; i++) {
+					IloLinearNumExpr expr = cplex.linearNumExpr();
+					for (int j = 0; j < V.length; j++) {
 						if (i != j) {
-							if (n + i != j)
-								expr.addTerm(-1.0, x[n + i][j][k]);
+							expr.addTerm(1.0, x[i][j][k]);
 						}
 					}
-					cplex.addEq(expr, 0.0, "Constraint3");
+					
+					for (int j = 0; j < V.length; j++) {
+						if (n+i != j)
+							expr.addTerm(-1.0, x[n + i][j][k]);
+					}
+					cplex.addEq(expr, 0.0, "Constraint4");
 				}
 			}
 
-			// Constraint 4: Start route at the origin depot.
+			// Constraint 5 Pesch: Flow constraint: Every Node from P union D (1..2*n)
+			// must have the same amount of edges going and edges going out.
+			// The Nodes 0 and 2n+1 (7) are not covered by this constraint, because
+			// the route should start/end there.
+			// Constraint 5 Cordeau
+			for (int k = 0; k < K.length; k++) {
+				for (int i = 0; i < V.length; i++) {
+					// Start und Zieldepot sind vom Flow Constraint ausgenommen.
+					if (i != 0) {
+						// Start und Zieldepot sind vom Flow Constraint ausgenommen.
+						if (i != 2*n+f+1) {
+							IloLinearNumExpr expr = cplex.linearNumExpr();
+							for (int j = 0; j < V.length; j++) {
+								if (i != j) {
+									expr.addTerm(1.0, x[j][i][k]);
+								}
+							}
+							for (int j = 0; j < V.length; j++) {
+								if (i != j) {
+									expr.addTerm(-1.0, x[i][j][k]);
+								}
+							}
+							cplex.addEq(expr, 0.0, "Constraint5");
+						}
+					}
+				}
+			}
+			
+			// Constraint 7 Pesch: Start route at the origin depot.
+			// Constraint 4 Cordeau
 			for (int k = 0; k < K.length; k++) {
 				IloLinearNumExpr expr = cplex.linearNumExpr();
-				for (int j = 0; j < N.length; j++) {
+				for (int j = 0; j < V.length; j++) {
 					if (j != 0) {
 						expr.addTerm(1.0, x[0][j][k]);
 					}
 				}
-				cplex.addEq(expr, 1.0, "Constraint4");
+				cplex.addEq(expr, 1.0, "Constraint7");
 			}
 
-			// Constraint 5: Flow constraint: Every Node from P union D (1..2*n)
-			// must have the same amount of edges going and edges going out.
-			// The Nodes 0 and 2n+1 (7) are not covered by this constraint, because
-			// the route should start/end there.
-			for (int i = 1; i <= 2 * n; i++) {
-				for (int k = 0; k < K.length; k++) {
-					IloLinearNumExpr expr = cplex.linearNumExpr();
-					for (int j = 0; j < N.length; j++) {
-						if (i != j) {
-							expr.addTerm(1.0, x[j][i][k]);
-						}
-					}
-					for (int j = 0; j < N.length; j++) {
-						if (i != j) {
-							expr.addTerm(-1.0, x[i][j][k]);
-						}
-					}
-					cplex.addEq(expr, 0.0, "Constraint5");
-				}
-			}
-
-			// Constraint 6: End Route at destination depot.
+			// Constraint 9 Pesch: End Route at destination depot.
+			// Constraint 6 Cordeau
 			for (int k = 0; k < K.length; k++) {
 				IloLinearNumExpr expr = cplex.linearNumExpr();
-				for (int i = 0; i < N.length; i++) {
-					if (i != 2 * n + 1) {
-						expr.addTerm(1.0, x[i][2 * n + 1][k]);
+				for (int i = 0; i < V.length; i++) {
+					if (i != 2 * n + f + 1) {
+						expr.addTerm(1.0, x[i][2 * n + f + 1][k]);
 					}
 				}
-				cplex.addEq(expr, 1.0, "Constraint6");
+				cplex.addEq(expr, 1.0, "Constraint9");
 			}
 
 			// Kontinuirliche Variable B_ik für die Zeit, an der Truck seinen
 			// Service an Knoten i beginnt.
-			B = new IloNumVar[N.length][K.length];
-			for (int i = 0; i < N.length; i++) {
+			B = new IloNumVar[V.length][K.length];
+			for (int i = 0; i < V.length; i++) {
 				for (int k = 0; k < K.length; k++) {
 					B[i][k] = cplex.numVar(0, 1440, "ServiceTimeB(i" + i + ";k" + k + ")");
 				}
@@ -182,15 +209,15 @@ public class model {
 			// nachdem der Service an Knoten i abgeschlossen wurde und der
 			// LKW von i nach j gefahren ist.
 			double M;
-			for (int i = 0; i < N.length; i++) {
-				for (int j = 0; j < N.length; j++) {
+			for (int i = 0; i < V.length; i++) {
+				for (int j = 0; j < V.length; j++) {
 					if (i != j) {
 						for (int k = 0; k < K.length; k++) {
 							IloLinearNumExpr expr = cplex.linearNumExpr();
-							M = Math.max(0, N[i].getEndServiceTime() + N[i].getServiceDuration() + t[i][j]
-									- N[j].getBeginServiceTime());
+							M = Math.max(0, V[i].getEndServiceTime() + V[i].getServiceDuration() + t[i][j]
+									- V[j].getBeginServiceTime());
 							expr.addTerm(1.0, B[i][k]);
-							expr.setConstant(N[i].getServiceDuration() + t[i][j] - M);
+							expr.setConstant(V[i].getServiceDuration() + t[i][j] - M);
 							expr.addTerm(M, x[i][j][k]);
 							cplex.addGe(B[j][k], expr, "Constraint7");
 						}
@@ -199,8 +226,8 @@ public class model {
 			}
 
 			// Definition Variable Q_ik: Load of vehicle k after visiting node i.
-			IloNumVar[][][] Q = new IloNumVar[N.length][4][K.length];
-			for (int i = 0; i < N.length; i++) {
+			IloNumVar[][][] Q = new IloNumVar[V.length][4][K.length];
+			for (int i = 0; i < V.length; i++) {
 				for (int r = 0; r <= 3; r++) {
 					for (int k = 0; k < K.length; k++) {
 						Q[i][r][k] = cplex.numVar(0, K[k].getCapacity()[r], "Q(i" + i + ";r" + r + ";k" + k + ")");
@@ -214,15 +241,15 @@ public class model {
 			// Version von Cordeau: Ist schneller als die Version von Pesch.
 			// Liefert das selbe Ergebnis wie Pesch.
 			double W;
-			for (int i = 0; i < N.length; i++) {
-				for (int j = 0; j < N.length; j++) {
+			for (int i = 0; i < V.length; i++) {
+				for (int j = 0; j < V.length; j++) {
 					if (i != j) {
 						for (int k = 0; k < K.length; k++) {
 							for (int r = 0; r <= 3; r++) {
-								W = Math.min(K[k].getCapacity()[r], K[k].getCapacity()[r] + N[i].getLoad()[r]);
+								W = Math.min(K[k].getCapacity()[r], K[k].getCapacity()[r] + V[i].getLoad()[r]);
 								IloLinearNumExpr expr = cplex.linearNumExpr();
 								expr.addTerm(1.0, Q[i][r][k]);
-								expr.setConstant(N[j].getLoad()[r] - W);
+								expr.setConstant(V[j].getLoad()[r] - W);
 //								expr.setConstant(N[j].getLoad()[r] - 100);
 								expr.addTerm(W, x[i][j][k]);
 //								expr.addTerm(100, x[i][j][k]);
@@ -238,10 +265,10 @@ public class model {
 			for (int k = 0; k < K.length; k++) {
 			//Modell geändert: Im Original wird anstatt N=PuD N=PuDu{0, 2n+1} genommen.
 			//Start und Zieldepot sind in dieser Variante inbegriffen.
-				for (int i = 0; i < N.length; i++) {
+				for (int i = 0; i < V.length; i++) {
 				// Hier das selbe wie beim vorherigen Kommentar. Für Start- und Zielknoten 
 				// gilt diese Bedingung auch.
-					for (int j = 0; j < N.length; j++) {
+					for (int j = 0; j < V.length; j++) {
 						if (i != j) {
 							for (int r = 0; r <= 3; r++) {
 //								if (N[i].getLoad()[r] != 0) {
@@ -251,7 +278,7 @@ public class model {
 									expr1.addTerm(1.0, Q[i][r][k]);
 									// Hier ist die big M notation abggeändert.
 									// Groß M soll eigentlich die Kapazität des LKW sein, ist jetzt aber 100
-									expr1.setConstant(N[j].getLoad()[r] + K[k].getCapacity()[r]);
+									expr1.setConstant(V[j].getLoad()[r] + K[k].getCapacity()[r]);
 //									expr1.setConstant(N[j].getLoad()[r] + 100);
 									expr1.addTerm(-K[k].getCapacity()[r], x[i][j][k]);
 //									expr1.addTerm(-100, x[i][j][k]);
@@ -259,7 +286,7 @@ public class model {
 									
 									IloLinearNumExpr expr2 = cplex.linearNumExpr();
 									expr2.addTerm(1.0, Q[i][r][k]);
-									expr2.setConstant(N[j].getLoad()[r] - K[k].getCapacity()[r]);
+									expr2.setConstant(V[j].getLoad()[r] - K[k].getCapacity()[r]);
 //									expr2.setConstant(N[j].getLoad()[r] - 100);
 									expr2.addTerm(K[k].getCapacity()[r], x[i][j][k]);
 //									expr2.addTerm(100, x[i][j][k]);
@@ -277,7 +304,7 @@ public class model {
 			double lMaxRideTime = 360;
 
 			// Definition L_i^k: The ride time of user i on vehicle k.
-			IloNumVar[][] L = new IloNumVar[N.length][K.length];
+			IloNumVar[][] L = new IloNumVar[V.length][K.length];
 			for (int i = 1; i <= n; i++) {
 				for (int k = 0; k < K.length; k++) {
 					L[i][k] = cplex.numVar(0, lMaxRideTime, "L(i" + i + ";k" + k + ")");
@@ -293,7 +320,7 @@ public class model {
 					IloLinearNumExpr expr = cplex.linearNumExpr();
 					expr.addTerm(1.0, B[n + i][k]);
 					expr.addTerm(-1.0, B[i][k]);
-					expr.setConstant(-N[i].getServiceDuration());
+					expr.setConstant(-V[i].getServiceDuration());
 					cplex.addEq(L[i][k], expr, "Constraint9");
 				}
 			}
@@ -309,10 +336,10 @@ public class model {
 
 			// Constraint 11 Cordeau: Knoten müssen innerhalb ihrer Servicezeit besucht werden.
 			// Constraint 20 Pesch
-			for (int i = 0; i < N.length; i++) {
+			for (int i = 0; i < V.length; i++) {
 				for (int k = 0; k < K.length; k++) {
-					cplex.addLe(N[i].getBeginServiceTime(), B[i][k], "Constraint11_1");
-					cplex.addLe(B[i][k], N[i].getEndServiceTime(), "Constraint11_2");
+					cplex.addLe(V[i].getBeginServiceTime(), B[i][k], "Constraint11_1");
+					cplex.addLe(B[i][k], V[i].getEndServiceTime(), "Constraint11_2");
 				}
 			}
 
@@ -342,7 +369,7 @@ public class model {
 
 			// Pesch
 			for (int k = 0; k < K.length; k++) {
-				for (int i = 0; i < N.length; i++) {
+				for (int i = 0; i < V.length; i++) {
 					for (int r = 0; r <= 3; r++) {
 						cplex.addLe(0.0, Q[i][r][k], "Constraint11_1");
 						cplex.addLe(Q[i][r][k], K[k].getCapacity()[r], "Constraint11_2");
@@ -390,7 +417,7 @@ public class model {
 			// Pesch
 			for (int k = 0; k < K.length; k++) {
 				for (int r = 0; r <= 3; r++) {
-					cplex.addEq(Q[N.length - 1][r][k], 0.0, "Constraint15");
+					cplex.addEq(Q[V.length - 1][r][k], 0.0, "Constraint15");
 				}
 			}
 
@@ -430,60 +457,34 @@ public class model {
 	}
 
 	/**
-	 * Automatically generates random Nodes.
-	 * 
-	 * @param numberOfNodes
-	 */
-	public static void autoGenerateNodes(int numberOfNodes) {
-		// Auto generate Nodes:
-		n = numberOfNodes;
-
-		N = new Node[2 * n + 2];
-		// Start Node is the origin depot.
-		N[0] = new Node(Math.random() * 4 + 1, Math.random() * 4 + 1, 0, 1440, new int[] { 0, 0, 0, 0 }, 0);
-
-		// Pick up nodes 1..n
-		for (int i = 1; i <= n; i++) {
-			N[i] = new Node(Math.random() * 4 + 1, Math.random() * 4 + 1, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
-		}
-
-		// Drop down nodes n+1..2n
-		for (int i = n + 1; i <= 2 * n; i++) {
-			N[i] = new Node(Math.random() * 4 + 1, Math.random() * 4 + 1, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
-		}
-
-		// Destination Node 2n+1 is the last node.
-		N[2 * n + 1] = new Node(Math.random() * 4 + 1, Math.random() * 4 + 1, 0, 1440, new int[] { 0, 0, 0, 0 }, 0);
-	}
-
-	/**
 	 * Generate a default set of Nodes.</br>
 	 * 3 pickup locations, 3 dropdown locations, the start node and a end node will
 	 * be created.
 	 */
 	public static void setDefaultNodes() {
 		n = 5;
+		f = 0;
 
-		N = new Node[12];
+		V = new Node[12];
 		// The start node.
-		N[0] = new Node(1, 2, 0, 1440, new int[] { 0, 0, 0, 0 }, 0);
+		V[0] = new Node(1, 2, 0, 1440, new int[] { 0, 0, 0, 0 }, 0);
 
 		// The pick up nodes.
-		N[1] = new Node(1, 1, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
-		N[2] = new Node(1, 4, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
-		N[3] = new Node(4, 3, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
-		N[4] = new Node(2, 2, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
-		N[5] = new Node(2, 4, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
+		V[1] = new Node(1, 1, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
+		V[2] = new Node(1, 4, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
+		V[3] = new Node(4, 3, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
+		V[4] = new Node(2, 2, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
+		V[5] = new Node(2, 4, 0, 1440, new int[] { 1, 0, 0, 0 }, 30);
 
 		// The drop down nodes.
-		N[6] = new Node(4, 1, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
-		N[7] = new Node(4, 4, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
-		N[8] = new Node(1, 3, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
-		N[9] = new Node(3, 4, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
-		N[10] = new Node(3, 1, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
+		V[6] = new Node(4, 1, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
+		V[7] = new Node(4, 4, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
+		V[8] = new Node(1, 3, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
+		V[9] = new Node(3, 4, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
+		V[10] = new Node(3, 1, 0, 1440, new int[] { -1, 0, 0, 0 }, 30);
 
 		// The end depot.
-		N[11] = new Node(3, 2, 0, 1440, new int[] { 0, 0, 0, 0 }, 0);
+		V[11] = new Node(3, 2, 0, 1440, new int[] { 0, 0, 0, 0 }, 0);
 	}
 
 	private static void solveModel() {
@@ -504,9 +505,9 @@ public class model {
 						System.out.print("\t" + i);
 					}
 					System.out.println();
-					for (int i = 0; i < N.length; i++) {
+					for (int i = 0; i < V.length; i++) {
 						System.out.print(i + "\t");
-						for (int j = 0; j < N.length; j++) {
+						for (int j = 0; j < V.length; j++) {
 							if (i != j) {
 								// Möglicherweise müssen Werte gerundet werden.
 								if (cplex.getValue(x[i][j][k]) == 0) {
@@ -537,10 +538,10 @@ public class model {
 					System.out.println();
 					nextNode = getNextNode(0, k);
 					System.out.println("Knoten\txPosition\tyPosition");
-					System.out.println("0\t" + N[0].getxPosition() + "\t" + N[0].getyPosition());
+					System.out.println("0\t" + V[0].getxPosition() + "\t" + V[0].getyPosition());
 					while (nextNode != 0) {
 						System.out.println(
-								nextNode + "\t" + N[nextNode].getxPosition() + "\t" + N[nextNode].getyPosition());
+								nextNode + "\t" + V[nextNode].getxPosition() + "\t" + V[nextNode].getyPosition());
 						nextNode = getNextNode(nextNode, k);
 					}
 					System.out.println();
